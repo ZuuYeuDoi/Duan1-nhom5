8b8345cfedbeba21f6205a97b1e883a4ec605ca0
Hoàng Quân <hoangquan@Hoang-Quan-2.local>
2024-11-26 10:57:49 +0700

    Thêm dropdown icon user

--- view/header.php
+++ view/header.php
@@ -10,11 +10,59 @@
     <link href="./view/css/responsives.css" rel="stylesheet" type="text/css" media="all">
     <link rel="stylesheet" href="./view/css/font-awesome.min.css">
     <link href="./view/css/cf-stylesheet.css" rel="stylesheet" type="text/css" media="all">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 
 </head>
+<style>
+   /* CSS cho phần tử chứa account icon */
+.search.fixacc {
+    position: relative;
+}
 
+/* Dropdown menu ẩn mặc định */
+.dropdown-menu {
+    display: none; /* Ẩn menu khi chưa hover */
+    position: absolute;
+    top: 30px; /* Điều chỉnh khoảng cách từ icon */
+    left: 0;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    z-index: 10;
+}
+
+.dropdown-menu ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+.dropdown-menu ul li {
+    padding: 8px 16px;
+}
+
+.dropdown-menu ul li a {
+    text-decoration: none;
+    color: #333;
+    display: block;
+}
+
+.dropdown-menu ul li a:hover {
+    background-color: pink;
+}
+
+/* Hiển thị dropdown khi hover vào icon tài khoản hoặc dropdown */
+.search.fixacc:hover .dropdown-menu,
+.cart-head:hover + .dropdown-menu {
+    display: block;
+}
+
+
+</style>
 <body>
 
     <body class="stBody stHome index">
@@ -47,8 +95,9 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
-                                        <a   href="index.php?act=trangchu" title="Trang chủ">Trang chủ</a>
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                        <a href="index.php?act=trangchu" title="Trang chủ">Trang chủ</a>
                                         <ul class="sub-menu">
 
 
@@ -62,7 +111,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="index.php?act=gioithieu" title="Giới thiệu">Giới thiệu</a>
                                         <ul class="sub-menu">
 
@@ -77,7 +127,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/shop-verus" title="Shop ">Rượu Vang </a>
                                         <!-- <ul class="sub-menu">
                                             
@@ -136,7 +187,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/shop-verus" title="Bộ sưu tập">Rượu Mạnh</a>
                                         <!-- <ul class="sub-menu">
                                             
@@ -195,7 +247,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/contact" title="Liên hệ">Liên hệ</a>
                                         <ul class="sub-menu">
 
@@ -215,7 +268,8 @@
                             </nav>
                             <div class="search" id="sea">
                                 <button type="button" data-show="#search" id="search-des">
-                                    <img src="./view/images/iconfinder-search-858732.png" alt="search" title="Search" style="
+                                    <img src="./view/images/iconfinder-search-858732.png" alt="search" title="Search"
+                                        style="
                 padding-top: 3px;
             ">
                                 </button>
@@ -224,24 +278,31 @@
 
                                     <form class="search-fr" action="index.php?act=product">
                                         <div class="form-input">
-                                            <input name="kw" id="search" placeholder="Tìm kiếm..." value="" type="text" required="required">
+                                            <input name="kw" id="search" placeholder="Tìm kiếm..." value="" type="text"
+                                                required="required">
                                             <button type="submit" name="timkiem">
                                                 <i class="fa fa-search"></i>
                                             </button>
                                         </div>
                                     </form>
-
-
                                 </div>
                             </div>
 
                             <div class="search fixacc">
                                 <a class="cart-head" href="index.php?act=login" style="position:relative;">
-                                    <img src="./view/images/iconfinder-41-user-2123927.png" alt="Account" title="Account" style="padding-top: 6px;margin-right: 8px;
-            ">
+                                    <img src="./view/images/iconfinder-41-user-2123927.png" alt="Account"
+                                        title="Account" style="padding-top: 6px;margin-right: 8px;">
                                 </a>
-
+                                <!-- Dropdown Menu -->
+                                <div class="dropdown-menu">
+                                    <ul>
+                                        <li><a href="#">Profile</a></li>
+                                        <li><a href="index.php?act=logout">Logout</a></li>
+                                    </ul>
+                                </div>
                             </div>
+
+
 
 
                             <div class="search">
@@ -252,6 +313,8 @@
                                 </a>
 
                             </div>
+
+
 
 
 
@@ -270,7 +333,8 @@
                                 <img style="max-width: 90px;" src="images/logo.png" alt="logo ">
                             </a>
                             <form class="search-fr" action="/search">
-                                <input name="q" id="search2" placeholder="Tìm kiếm..." value="" type="text" required="required">
+                                <input name="q" id="search2" placeholder="Tìm kiếm..." value="" type="text"
+                                    required="required">
                                 <button type="submit">
                                     <i class="fa fa-search"></i>
                                 </button>
@@ -284,7 +348,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/">Trang chủ</a>
                                         <ul class="sub-menu">
 
@@ -299,7 +364,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/about-us">Giới thiệu</a>
                                         <ul class="sub-menu">
 
@@ -314,13 +380,16 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/shop-verus">Shop </a>
                                         <ul class="sub-menu">
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
-                                                <a href="shop-verus?q=collections:2362410%20AND%20vendor:(Verus)&amp;page=1">Verus</a>
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                                <a
+                                                    href="shop-verus?q=collections:2362410%20AND%20vendor:(Verus)&amp;page=1">Verus</a>
                                                 <ul class="sub-menu">
 
 
@@ -330,8 +399,10 @@
 
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
-                                                <a href="shop-verus?q=collections:2362410%20AND%20vendor:(Minirus)&amp;page=1">Minirus</a>
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                                <a
+                                                    href="shop-verus?q=collections:2362410%20AND%20vendor:(Minirus)&amp;page=1">Minirus</a>
                                                 <ul class="sub-menu">
 
 
@@ -341,8 +412,10 @@
 
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
-                                                <a href="shop-verus?q=collections:2362410%20AND%20vendor:(Merchandise)&amp;page=1">Merchandise</a>
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                                <a
+                                                    href="shop-verus?q=collections:2362410%20AND%20vendor:(Merchandise)&amp;page=1">Merchandise</a>
                                                 <ul class="sub-menu">
 
 
@@ -352,7 +425,8 @@
 
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                                 <a href="/rebel-nerd">Rebel Nerd</a>
                                                 <ul class="sub-menu">
 
@@ -373,12 +447,14 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/shop-verus">Bộ sưu tập</a>
                                         <ul class="sub-menu">
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                                 <a href="/world-cup-2022">World Cup 2022</a>
                                                 <ul class="sub-menu">
 
@@ -389,7 +465,8 @@
 
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                                 <a href="/verus-x-lien-quan-1"> x Liên Quân</a>
                                                 <ul class="sub-menu">
 
@@ -400,7 +477,8 @@
 
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                                 <a href="/verus-x-biti-s-hunter"> x Biti's Hunter</a>
                                                 <ul class="sub-menu">
 
@@ -411,7 +489,8 @@
 
 
 
-                                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                            <li
+                                                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                                 <a href="/zodiac-12-cung-hoang-dao">Zodiac / 12 Cung Ho�&nbsp;ng Đạo</a>
                                                 <ul class="sub-menu">
 
@@ -432,7 +511,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
+                                    <li
+                                        class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children">
                                         <a href="/contact">Liên hệ</a>
                                         <ul class="sub-menu">
 
@@ -450,7 +530,8 @@
 
 
 
-                                    <li class="menu-item menu-item-type-post_type menu-item-object-page "><a href="/?act=login"> Register / Sign in</a></li>
+                                    <li class="menu-item menu-item-type-post_type menu-item-object-page "><a
+                                            href="/?act=login"> Register / Sign in</a></li>
 
 
 
